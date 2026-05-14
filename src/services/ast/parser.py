f293ac30b86bf68d9fe490993a48fb8029f555d6
quilix11 <nazarus8095@gmail.com>
2026-05-14 19:38:08 +0300

    refactor ast folder and make parser

---
+++ src/services/ast/parser.py
@@ -0,0 +1,17 @@
+import ast
+from reader import file_readers
+
+def parser():
+    files_data = file_readers()
+    trees = {}
+
+    for path, code in files_data.items():
+            try:
+                tree = ast.parse(code, filename=path)
+                trees[path] = ast.dump(tree, indent=2)
+            except SyntaxError as e:
+                trees[path] = e
+                
+    return trees
+
+print(parser())
